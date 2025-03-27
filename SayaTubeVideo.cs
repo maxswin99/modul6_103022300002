e1e1fae9bfaccd4c792166a462ea3605bc120289
maxswin99 <andreaschristianfirga06@gmail.com>
2025-03-27 10:36:49 +0700

    penambahan design by contract pada semua kelas

---
+++ SayaTubeVideo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+
+public class SayaTubeVideo
+{
+    private int playCount;
+    private string title;
+
+    public SayaTubeVideo(string title)
+    {
+        
+        Debug.Assert(!string.IsNullOrEmpty(title), "Judul video tidak boleh null atau kosong.");
+        Debug.Assert(title.Length <= 200, "Judul video maksimal 200 karakter.");
+
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        
+        Debug.Assert(count > 0, "Play count harus bilangan positif.");
+        Debug.Assert(count <= 25000000, "Penambahan play count maksimal 25.000.000.");
+
+        try
+        {
+            checked
+            {
+                this.playCount += count;
+            }
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("ERROR: Terjadi overflow saat menambahkan play count.");
+        }
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine($"Judul Video: {this.title}");
+        Console.WriteLine($"Jumlah Play Count: {this.playCount}");
+    }
+
+    
+    public void PrintAllVideoPlaycount()
+    {
+        Debug.Assert(this.playCount >= 0, "Jumlah play count tidak boleh negatif.");
+        Console.WriteLine($"Total Play Count: {this.playCount}");
+    }
+}
---
+++ modul6_103022300002/SayaTubeUser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+public class SayaTubeUser
+{
+    private string username;
+    private List<SayaTubeVideo> uploadedVideos;
+
+    public SayaTubeUser(string username)
+    {
+        
+        Debug.Assert(!string.IsNullOrEmpty(username), "Username tidak boleh null atau kosong.");
+        Debug.Assert(username.Length <= 100, "Panjang username maksimal 100 karakter.");
+
+        this.username = username;
+        this.uploadedVideos = new List<SayaTubeVideo>();
+    }
+
+    public void AddVideo(SayaTubeVideo video)
+    {
+        
+        Debug.Assert(video != null, "Video yang ditambahkan tidak boleh null.");
+        uploadedVideos.Add(video);
+    }
+
+    public void PrintAllVideos()
+    {
+        Console.WriteLine($"Username: {this.username}");
+        foreach (var video in uploadedVideos)
+        {
+            video.PrintVideoDetails();
+        }
+    }
+}
